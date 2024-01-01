528a080c38b94618fb509804e58a92cbddbe59fc
rinajuniar <149214199+rinajuniar@users.noreply.github.com>
2024-01-01 15:24:53 +0700

    Add files via upload

---
+++ Tabs/home.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+@st.cache_data
+def app():
+    st.title("Aplikasi Prediksi Penyakit Jantung")
---
+++ Tabs/visualise.py
@@ -0,0 +1,27 @@
+import warnings
+import matplotlib.pyplot as plt 
+import streamlit as st 
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
+
+from web_functions import train_model
+
+def plot_confusion_matrix(model, x, y):
+    y = model.predict(x)
+    confusion_matrix(y,y)
+    sns.heatmap((confusion_matrix(y,y)), annot=True, cmap="YlGnBu" ,fmt='g')
+plt.title('Confusion matrix', y=1.05)
+plt.ylabel('Actual label')
+plt.xlabel('Predicted label')
+
+def app(df, x, y):
+    warnings.filterwarnings('ignore')
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+
+
+    st.title("Visualisasi")
+    model, score = train_model(x, y)
+    
+    if st.checkbox("Plot"):
+        plot_confusion_matrix(model, x, y)
+        st.pyplot()
